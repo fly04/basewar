f93127aa9d8ff3a3336dfed9ef8319e19a745405
Luka Najjar <coksilaweed@gmail.com>
2021-10-12 09:44:08 +0200

    Implements authentification

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-//To get access to environment variables stored in .env
+// To get access to environment variables stored in .env
 require('dotenv').config();
 
-//Environment variables (add validation?)
+// Environment variables (add validation?)
 exports.dbUrl = process.env.DATABASE_URL || 3000;
+
+// JWT signing key
+exports.secretKey = process.env.SECRET_KEY || 'changeme';
+exports.bcryptCostFactor = 10;
