baa36482eb0aa61378f42b45d4fe83feb658465d
Thomas Robert <thomas.robert@heig-vd.ch>
2021-11-15 11:02:36 +0100

    Update test for users

--- spec/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require("chai");
-
-describe("POST /users", function () {
-	it("should create a user");
-});
-
-describe("GET /users", function () {
-	it("should retrieve the list of users");
-});
